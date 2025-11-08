ec681955da3060a15774e1b2108e61c689d4f68d
Bjark9 <niels.martin03@gmail.com>
2025-11-08 10:47:50 +0100

    added interface IBankAccount

--- Martin_Niels/Account.cs
+++ Martin_Niels/Account.cs
@@ -46,16 +46,27 @@ namespace BankApplication_Cours
 {
     public interface IAccount
     {
+        // Interface "cache" les méthodes
+        // To achieve security - hide certain details and only show the important details of an object (interface).
         public double Balance { get; }
         public void Withdraw(double amount);
         public void Deposit(double amount);
     }
-    abstract class Account : IAccount
+
+    public interface IBankAccount : IAccount
+    {
+        public void ApplyInterest();
+        public Person Owner { get; } // accès en lecture seulement
+        public string AccNumber { get; }
+        
+        
+    }
+    abstract class Account : IBankAccount
     {
         // Cette classe va être la classe parente dont SavingsAccount et CurrentAccount vont hériter
         public required string AccNumber { get; set; }
         public double Balance { get; private set; }
-        public required BankApplication_Cours.Person Owner { get; set; }
+        public required BankApplication_Cours.Person Owner { get; set; } // Is an object?
     
         public virtual void Withdraw(double amount)
         {
--- Martin_Niels/Person.cs
+++ Martin_Niels/Person.cs
@@ -1,6 +1,6 @@
 namespace BankApplication_Cours
 {
-    class Person
+    public class Person
     {
         public required string FirstName { get; set; }
         public required string LastName { get; set; }

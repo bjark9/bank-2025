d11c12e00f5a22ce5e618a4462db17b3f52eb511
Bjark9 <niels.martin03@gmail.com>
2025-11-15 11:00:13 +0100

    eventing

--- Martin_Niels/CurrentAccount.cs
+++ Martin_Niels/CurrentAccount.cs
@@ -3,14 +3,14 @@ namespace BankApplication_Cours
     class CurrentAccount : Account
     {
         private double creditLine; // field
-        public CurrentAccount(string AccNumber, Person Owner):base(AccNumber,Owner) { }
-        public CurrentAccount(string AccNumber, Person Owner, double Balance):base(AccNumber,Owner,Balance) {}
+        public CurrentAccount(string AccNumber, Person Owner) : base(AccNumber, Owner) { }
+        public CurrentAccount(string AccNumber, Person Owner, double Balance) : base(AccNumber, Owner, Balance) { }
         public double CreditLine // property, combination of variable and method
         {
             get => creditLine; // get -> returns the value of the variable creditLine
             // set -> assigns a value to the variable creditLine
             // If we want to calculate with "creditline" -> use the property (not the field) or this will bypass the validation
-            set 
+            set
             {
                 if (value <= 0)
                 {
@@ -30,6 +30,15 @@ namespace BankApplication_Cours
                 return 0.0975; // 9.75%
             }
 
+        }
+
+        // Déclencher l'événement quand le compte passe en dessous de 0.
+        public void CheckBalance()
+        {
+            if (Balance < 0)
+            {
+                OnNegativeBalance();
+            }
         }
     }
 }

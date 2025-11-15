0b6f32682057ce404ffd1d98ac23e8b57165cf75
Bjark9 <niels.martin03@gmail.com>
2025-11-15 09:02:06 +0100

    exceptioning on property

--- Martin_Niels/CurrentAccount.cs
+++ Martin_Niels/CurrentAccount.cs
@@ -2,9 +2,23 @@ namespace BankApplication_Cours
 {
     class CurrentAccount : Account
     {
+        private double creditLine; // field
         public CurrentAccount(string AccNumber, Person Owner):base(AccNumber,Owner) { }
         public CurrentAccount(string AccNumber, Person Owner, double Balance):base(AccNumber,Owner,Balance) {}
-        public double CreditLine { get; set; }
+        public double CreditLine // property, combination of variable and method
+        {
+            get => creditLine; // get -> returns the value of the variable creditLine
+            // set -> assigns a value to the variable creditLine
+            // If we want to calculate with "creditline" -> use the property (not the field) or this will bypass the validation
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(CreditLine), "CreditLine has to be over 0.");
+                }
+                creditLine = value;
+            }
+        }
         protected override double CalculInterest()
         {
             if (Balance > 0)
